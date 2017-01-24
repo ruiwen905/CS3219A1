f32e9a8e1abb11742cd426ff2e72e37bcc7b5c05
Seow Weijie <sweijie94@gmail.com>
2017-01-24 22:01:51 +0800

    Added input.txt and implemented file reading from input.txt

--- ADT/ADT.java
+++ ADT/ADT.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -36,12 +38,35 @@ public class ADT {
 		
 		ArrayList<String> inputLines = readFromFile(inputFileName);
 		
-
+		for (int i = 0; i < inputLines.size(); i++) {
+			System.out.println(inputLines.get(i));
+		}
 		
 	}
 
 	private static ArrayList<String> readFromFile(String inputFileName) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<String> records = new ArrayList<String>();
+		  try
+		  {
+		    BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+		    String line;
+		    while ((line = reader.readLine()) != null)
+		    {
+		      records.add(line);
+		    }
+		    reader.close();
+		    return records;
+		  }
+		  catch (Exception e)
+		  {
+		   /* System.err.format("Exception occurred trying to read '%s'.", inputFileName);
+		    e.printStackTrace();
+		    return null; */
+			  System.out.println("Exception occurred trying to read "+ inputFileName + ".");
+			  System.exit(1);
+			  
+			  return null;
+		  }
 	}
 }
