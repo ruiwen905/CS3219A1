6ee0c1e82347295de5004c70dd4d2eed52730c15
Seow Weijie <sweijie94@gmail.com>
2017-01-30 11:22:45 +0800

    Remove unwanted imports

--- ADT/CircularShift.java
+++ ADT/CircularShift.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * This is the Circular Shift Module for KWIC program.
@@ -33,41 +31,7 @@ public class CircularShift {
 
 	/**
 	 * Perform circular shift on the lines
-	 */
-	
-	/*
-	private void circularShift() {
-		Queue<String> queue = new LinkedList<String>();
-
-		// Going through all the lines
-		for (int i = 0; i < inputLines.size(); i++) {
-			queue.offer(inputLines.get(i));
-		}
-
-		// Using a queue to go through all possible orders of circular shift 
-		// by appending the first word to the end of the string
-		while (!queue.isEmpty()) {
-
-			String line = queue.poll();
-			
-			// Discarding lines that is duplicated
-			if (!shiftedLines.contains(line)) {
-				
-				// Splitting each line into words
-				String[] wordsInLine = line.split(" ");
-
-				String firstWord = wordsInLine[0];
-				
-				// Add the shifted line if the first word is not a ignored word
-				if (!wordsToIgnore.contains(firstWord.toLowerCase())) {
-					shiftedLines.add(line);
-				}
-				String modifiedLine = appendFirstWordToEnd(wordsInLine, firstWord);
-				queue.offer(modifiedLine);
-			}
-		}
-	} */
-	
+	 */	
 	private void circularShift() {
 		
 		// Going through all the lines
@@ -86,6 +50,7 @@ public class CircularShift {
 				// Add the shifted line if the first word is not a ignored word
 				if (!wordsToIgnore.contains(firstWord.toLowerCase())) {
 					
+					// Forming the correct line by appending the words in order
 					String keywordLine = words[j];
 					
 					int start = (j + 1) % lengthOfLine;
@@ -99,22 +64,5 @@ public class CircularShift {
 				}
 			}
 		}
-	}
-
-	/**
-	 * Brings the first word to the end of the string
-	 */
-	private String appendFirstWordToEnd(String[] wordsInLine, String firstWord) {
-
-		String result = "";
-
-		for (int i = 1; i < wordsInLine.length; i++) {
-			result += wordsInLine[i];
-			result += " ";
-		}
-
-		result += firstWord;
-
-		return result;
 	}
 }
